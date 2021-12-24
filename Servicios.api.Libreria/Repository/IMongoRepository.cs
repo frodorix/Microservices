26dcca9faba5e8e819abb333bba7962ed6b15ae9
Edwin Frederick <el_frederick@hotmail.com>
2021-12-24 09:50:07 -0400

    new operations implemented for Generic MongoDB Repository: Insert, Delete, Update

--- Servicios.api.Libreria/Repository/IMongoRepository.cs
+++ Servicios.api.Libreria/Repository/IMongoRepository.cs
@@ -5,6 +5,12 @@ namespace Servicios.api.Libreria.Repository
     public interface IMongoRepository<TDocument> where TDocument : IDocument
     {
         Task<IEnumerable<TDocument>>  GetAll();
+        Task<TDocument> GetById(string id);
+        Task InsertDocument(TDocument document);
+        Task UpdateDocument(TDocument document);
+
+        Task DeleteById(string id);
+
 
     }
 }
