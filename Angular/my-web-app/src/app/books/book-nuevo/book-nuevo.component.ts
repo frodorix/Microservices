3a7d303508b9c2c867fbc5b27d17050317367fed
Edwin Frederick <el_frederick@hotmail.com>
2022-01-02 19:00:21 -0400

    Form validation

--- Angular/my-web-app/src/app/books/book-nuevo/book-nuevo.component.ts
+++ Angular/my-web-app/src/app/books/book-nuevo/book-nuevo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatOption } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
 import { from } from 'rxjs';
 import { BooksService } from '../books.service';
@@ -17,24 +18,24 @@ export class BookNuevoComponent implements OnInit {
   selectAutorTexto: string = '';
   fechaPublicacion: string = '';
 
-  constructor(private booksService: BooksService) {}
+  constructor(private booksService: BooksService , private dialogRef:MatDialog) {}
 
   ngOnInit(): void {}
 
   guardarLibro(form: NgForm) {
-    var b={
-      libroId: 1,
-      descripcion: form.value.descripcion,
-      titulo: form.value.titulo,
-      autor: this.selectAutorTexto,
-      precio: form.value.precio,
-      fechaPublicacion: new Date(this.fechaPublicacion),
-    };
-    console.log(b);
-
-    this.booksService.guardarLibro(b);
+    if (form.valid) {
+      var b = {
+        libroId: 1,
+        descripcion: form.value.descripcion,
+        titulo: form.value.titulo,
+        autor: this.selectAutorTexto,
+        precio: form.value.precio,
+        fechaPublicacion: new Date(this.fechaPublicacion),
+      };
+      this.booksService.guardarLibro(b);
+      this.dialogRef.closeAll();
+    }
   }
-
   selected(event: MatSelectChange) {
     this.selectAutorTexto = (event.source.selected as MatOption).viewValue;
   }
