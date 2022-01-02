3b6e25ad2f88385ecade0b80ff6467560a6f09ee
Edwin Frederick <el_frederick@hotmail.com>
2022-01-02 19:47:27 -0400

    ususcribe service

--- Angular/my-web-app/src/app/autores/autores.component.ts
+++ Angular/my-web-app/src/app/autores/autores.component.ts
@@ -1,6 +1,13 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
 import { Autor } from './autor.model';
 import { AutoresService } from './autores.service';
 
@@ -9,22 +16,27 @@ import { AutoresService } from './autores.service';
   templateUrl: './autores.component.html',
   styleUrls: ['./autores.component.css'],
 })
-export class AutoresComponent implements OnInit, AfterViewInit {
+export class AutoresComponent implements OnInit, AfterViewInit, OnDestroy {
   desplegarColumnas = ['nombre', 'apellido', 'gradoAcademico'];
   dataSource = new MatTableDataSource<Autor>();
   @ViewChild(MatSort)
   ordenamiento!: MatSort;
+  private autorSusbscription!: Subscription;
   constructor(private autoresService: AutoresService) {}
+
+  ngOnDestroy(): void {
+    this.autorSusbscription.unsubscribe();
+  }
   ngAfterViewInit(): void {
     this.dataSource.sort = this.ordenamiento;
   }
 
   ngOnInit(): void {
     this.autoresService.obtenerAutores();
-    this.autoresService.obtenerActualizarListener()
-    .subscribe((autores:Autor[])=>{
-      this.dataSource.data=autores;
-    });
-    //this.dataSource.data = this.autoresService.obtenerAutores();
+    this.autorSusbscription = this.autoresService
+      .obtenerActualizarListener()
+      .subscribe((autores: Autor[]) => {
+        this.dataSource.data = autores;
+      });
   }
 }
