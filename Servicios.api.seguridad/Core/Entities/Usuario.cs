18d6c6aa8549b815613233977cb4bdcfb0da3581
Edwin Frederick <el_frederick@hotmail.com>
2022-01-02 22:50:02 -0400

    Microservices Security: Creating DBContext for CoreIdentity / EF Core

---
+++ Servicios.api.seguridad/Core/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Servicios.api.seguridad.Core.Entities
+{
+    public class Usuario: IdentityUser
+    {
+        public string Nombre { get; set; }  
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}
---
+++ Servicios.api.seguridad/Core/Persistence/SeguridadContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Servicios.api.seguridad.Core.Entities;
+
+namespace Servicios.api.seguridad.Core.Persistence
+{
+    public class SeguridadContexto : IdentityDbContext<Usuario>
+    {
+        public SeguridadContexto(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
