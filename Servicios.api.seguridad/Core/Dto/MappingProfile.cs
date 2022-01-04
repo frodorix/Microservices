bc1411bdf5c6bcc47b93bfd93743e1f2f5de80c0
Edwin Frederick <el_frederick@hotmail.com>
2022-01-04 12:05:50 -0400

    .net 5 and DTO classes

---
+++ Servicios.api.seguridad/Core/Dto/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Servicios.api.seguridad.Core.Dto
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UsuarioDto, UsuarioDto>();
+        }
+    }
+}
--- Servicios.api.seguridad/Core/Dto/UsuarioDto.cs
+++ Servicios.api.seguridad/Core/Dto/UsuarioDto.cs
@@ -2,5 +2,10 @@
 {
     public class UsuarioDto
     {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
     }
 }
